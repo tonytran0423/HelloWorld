b3c22f6a94d67b5b59b28c1b996323373e50c3a2
tonytran0423 <tonytran0423@yahoo.com>
2017-01-11 08:40:39 -0500

    Attempted to add a print statement

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-    	//basic testing
         System.out.println("Hello World!"); // Display the string.
+        //System.out.println("Bye World!";)
+        
     }
 }
